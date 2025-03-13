3c132288662257beaf3b0e6d5b0cbd5786ba081f
Curry5004 <yunjin5004@naver.com>
2025-03-14 08:00:36 +0900

    * 25. 03. 14
    프로젝트 생성

---
+++ src/main/java/hello/login/domain/member/MemverRepository.java
@@ -0,0 +1,40 @@
+package hello.login.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Slf4j
+@Repository
+public class MemverRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long seq = 0L;
+
+    public Member save(Member member){
+        member.setId(++seq);
+
+        log.info("MemverRepository save. member = {}", member);
+        store.put(member.getId(), member);
+
+        return member;
+    }
+
+    public Member findById(long id){
+        return store.get(id);
+    }
+
+    public Optional<Member> findByLoginId(String loginId){
+        return findAll().stream().filter(a -> a.getLoginId().equals(loginId)).findFirst();
+    }
+
+    public List<Member> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    public  void  cleanStore(){
+        store.clear();
+    }
+
+}
